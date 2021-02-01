5fd7e89f9dd2519262c1f79df199dfd515776e29
Yu-jinKang <jgkyj@naver.com>
2021-02-01 12:18:15 +0900

    commit mma

--- MusicDiary/MusicDiary/Yujin/IntroViewController.swift
+++ MusicDiary/MusicDiary/Yujin/IntroViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 import FirebaseUI
+import SwiftyGif
 
 
 class IntroViewController: UIViewController, FUIAuthDelegate {
     
     //values
+    @IBOutlet weak var logoImageView: UIImageView! //logoImageView
     @IBOutlet weak var swipeLable: UILabel! //swipeLabel
     let authUI = FUIAuth.defaultAuthUI() //authUI
     var handle: AuthStateDidChangeListenerHandle! //handle
@@ -25,16 +27,27 @@ class IntroViewController: UIViewController, FUIAuthDelegate {
     //viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //logoImageView GIF start
+        do{
+            let gif = try UIImage(gifName: "spongeBob.gif")
+            self.logoImageView.setGifImage(gif, loopCount: -1)
+        } catch {
+            print("not loaded")
+        }
+        /* labelTapped Test
         let tap = UITapGestureRecognizer(target: self, action: #selector(labelTapped(_:)))
         swipeLable.addGestureRecognizer(tap)
         swipeLable.isEnabled = true
+         */
     }//viewDidLoad
     
     //viewDidDisappear
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
+        //firebase auth listener 삭제
         Auth.auth().removeStateDidChangeListener(handle)
+        //logoImageView GIF 멈춤
+        //self.logoImageView.stopAnimatingGif()
     }//viewDidDisappear
     
     //labelTapped
@@ -45,14 +58,18 @@ class IntroViewController: UIViewController, FUIAuthDelegate {
     //btnTouched
     @IBAction func btnTouched(_ sender: Any) {
         print("touched")
+        //핸들로 로그인 했는지 안했는지 추적
         handle = Auth.auth().addStateDidChangeListener { (auth, user) in
+            //만약 로그인 했다면
             if let currentUser = auth.currentUser {
-                //Logged User
+                //currentUser 정보 넘겨주기
+                print(currentUser.displayName)
+                //로그인한 유저 -> 다음 뷰로 넘어감
                 let vc = UIStoryboard(name: "YujinStoryboard", bundle: nil).instantiateViewController(identifier: "LoggedView")
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: false, completion:  nil)
             } else {
-                //Not Logged
+                //로그인 안한 유저 -> 로그인 화면이 뜸
                 self.authUI!.delegate = self
                   let providers: [FUIAuthProvider] = [
                   FUIGoogleAuth()
@@ -61,7 +78,6 @@ class IntroViewController: UIViewController, FUIAuthDelegate {
                 let authViewController = self.authUI!.authViewController()
                 authViewController.modalPresentationStyle = .fullScreen
                 authViewController.setNavigationBarHidden(true, animated: false)
-                
                 self.present(authViewController, animated: true, completion: nil)
             }
         }
@@ -70,6 +86,7 @@ class IntroViewController: UIViewController, FUIAuthDelegate {
     //logoutbtnTouched
     @IBAction func logoutbtnTouched(_ sender: Any) {
         print("logOut")
+        //로그아웃 시도
         do {
             try authUI?.signOut()
         } catch {
