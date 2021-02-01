3d7593b078d8c886a320523231b30c9c09d698c7
yeew3658 <yeew3658@gmail.com>
2021-02-01 09:36:55 +0900

    Setting story board

---
+++ MusicDiary/MusicDiary/Yewon/DiaryCollectionView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  MusicDiary
+//
+//  Created by 강예원 on 2021/01/28.
+//
+
+import UIKit
+
+class DiaryCollectionView:UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
