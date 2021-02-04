5464a0d337282a545062612bb1dd0b14b954dfee
Yu-jinKang <jgkyj@naver.com>
2021-02-04 21:29:18 +0900

    MusicStructure Added

---
+++ MusicDiary/MusicDiary/Yujin/MusicStructure.swift
@@ -0,0 +1,13 @@
+//
+//  SongStructure.swift
+//  MusicDiary
+//
+//  Created by 강유진 on 2021/02/04.
+//
+import UIKit
+
+struct MusicStruct {
+    var musicTitle: String?
+    var musicArtist: String?
+    var musicCoverUrl: URL?
+}
