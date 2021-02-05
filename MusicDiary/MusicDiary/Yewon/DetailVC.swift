5981bc26acea1e99849536ed1339b9ea71fb907e
yeew3658 <yeew3658@gmail.com>
2021-02-06 04:51:35 +0900

    yewon Main
    View Edit, Add DetailVC file

---
+++ MusicDiary/MusicDiary/Yewon/DetailVC.swift
@@ -0,0 +1,14 @@
+//
+//  DetailView.swift
+//  MusicDiary
+//
+//  Created by 강예원 on 2021/02/06.
+//
+
+import UIKit
+
+class DetailVC:UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
