105cf892299050e2a1ff23ea7da0572175b31191
madilyn <lollin0@naver.com>
2021-02-05 12:47:52 +0900

    chore: temp

--- MusicDiary/MusicDiary/Jungbin/DailyView/DailyViewController.swift
+++ MusicDiary/MusicDiary/Jungbin/DailyView/DailyViewController.swift
@@ -14,48 +14,54 @@ class DailyViewController: UIViewController {
     @IBOutlet weak var carousel: ScalingCarouselView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        setDatePicker()
     }
     
 }
-
-typealias CarouselDatasource = ViewController
-extension CarouselDatasource:UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
-    }
-    
-    
-}
-
-typealias CarouselDelegate = ViewController
-extension ViewController: UICollectionViewDelegate {
-    
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        
-        
-        carousel.didScroll()
-        
-        guard let currentCenterIndex = self.carousel.currentCenterCellIndex?.row else { return }
-        
-        print("current index: ", String(describing: currentCenterIndex))
-    }
-}
-
-private typealias ScalingCarouselFlowDelegate = ViewController
-extension ScalingCarouselFlowDelegate: UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        
-        return 0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        
-        return 0
-    }
-}
+//
+//typealias CarouselDatasource = ViewController
+//extension CarouselDatasource:UICollectionViewDataSource{
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+//
+//
+//        if let scalingCell = cell as? ScalingCarouselCell {
+//            scalingCell.mainView.backgroundColor = .red
+//            scalingCell.cornerRadius = 80
+//        }
+//
+//        DispatchQueue.main.async {
+//            cell.setNeedsLayout()
+//            cell.layoutIfNeeded()
+//        }
+//        return cell
+//}
+//
+//typealias CarouselDelegate = ViewController
+//extension ViewController: UICollectionViewDelegate {
+//
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        carousel.didScroll()
+//
+//        guard let currentCenterIndex = self.carousel.currentCenterCellIndex?.row else { return }
+//
+//        print("current index: ", String(describing: currentCenterIndex))
+//    }
+//}
+//
+//private typealias ScalingCarouselFlowDelegate = ViewController
+//extension ScalingCarouselFlowDelegate: UICollectionViewDelegateFlowLayout {
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//
+//        return 0
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//
+//        return 0
+//    }
+//}
