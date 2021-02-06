1d78ab71a3460928a093e99204c9e9802fd5ba2b
yeew3658 <yeew3658@gmail.com>
2021-02-07 07:57:40 +0900

    Add Delete Btn function

--- MusicDiary/MusicDiary/Yewon/DetailVC.swift
+++ MusicDiary/MusicDiary/Yewon/DetailVC.swift
@@ -18,6 +18,7 @@ class DetailVC:UIViewController {
     
     let db = Firestore.firestore()
     var contentData = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,18 +26,40 @@ class DetailVC:UIViewController {
 
         docRef.getDocument { (document, error) in
             if let document = document, document.exists {
-                let dataDescription = document.data()
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
                 print("Document data: \(dataDescription)")
+                self.contentData.append(dataDescription)
                 
             } else {
                 print("Document does not exist")
             }
         }
         
-
-        
     }
     
+    //delete docunet
+    @IBAction func deleteDoc(_ sender: UIButton) {
+        let alertVC = UIAlertController(title: "Delete", message: "정말 삭제할까요?", preferredStyle: .alert)
+        let action_cancel = UIAlertAction(title: "Cencel", style: .cancel, handler: nil)
+        let action_delete = UIAlertAction(title: "Delete", style: .default) { (action) in
+            do {
+                self.db.collection("Diary").document("IxLlj4mK2DKPIoBA9Qjp").collection("Contents").document("ytGhBqaCnQAdq3feUqhI").delete() { err in
+                    if let err = err {
+                        print("Error removing document: \(err)")
+                    } else {
+                        print("Document successfully removed!")
+                    }
+                }
+            print("delete")
+            self.dismiss(animated: true, completion: nil)
+            } catch {
+                
+            }
+        }
+        alertVC.addAction(action_cancel)
+        alertVC.addAction(action_delete)
+        self.present(alertVC, animated: true, completion: nil)
+    }
 }
 
 class DetailSub1VC: DetailVC {
