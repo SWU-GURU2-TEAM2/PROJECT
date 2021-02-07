bebb71523ac41e9e94669cfc337c00c3a559eed6
Yu-jinKang <jgkyj@naver.com>
2021-02-07 16:59:16 +0900

    loginDataFirebased

--- MusicDiary/MusicDiary/Yujin/IntroViewController.swift
+++ MusicDiary/MusicDiary/Yujin/IntroViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import FirebaseUI
 import SwiftyGif
-
+import FirebaseFirestore
 
 class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognizerDelegate {
     
@@ -16,13 +16,9 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
     @IBOutlet weak var logoImageView: UIImageView! //logoImageView
     @IBOutlet weak var swipeLable: UILabel! //swipeLabel
     let authUI = FUIAuth.defaultAuthUI() //authUI
+    let db = Firestore.firestore() //db
     var handle: AuthStateDidChangeListenerHandle! //handle
     var currentUID:String! //currentUID
-    //viewWillAppear
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
     
     //viewDidLoad
     override func viewDidLoad() {
@@ -35,6 +31,7 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
             print("not loaded")
         }
         
+        //swipeGestureControl
         let swipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipeGestured(_:)))
         swipeGestureRecognizer.direction = UISwipeGestureRecognizer.Direction.up
         self.view.addGestureRecognizer(swipeGestureRecognizer)
@@ -45,8 +42,6 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
         super.viewDidDisappear(animated)
         //firebase auth listener 삭제
         Auth.auth().removeStateDidChangeListener(handle)
-        //logoImageView GIF 멈춤
-        //self.logoImageView.stopAnimatingGif()
     }//viewDidDisappear
     
     //swipeGesture
@@ -57,6 +52,21 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
             if let currentUser = auth.currentUser {
                 //currentUser 정보 넘겨주기
                 //로그인한 유저 -> 다음 뷰로 넘어감
+                let docRef = self.db.collection("Users").document("\(currentUser.uid)")
+                docRef.getDocument { (document, error) in
+                    if let document = document, document.exists {
+                        let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                        print("Document data: \(dataDescription)")
+                    } else {
+                        print("Document does not exist")
+                        self.db.collection("Users").document("\(currentUser.uid)").setData(["userDiaryList" : [],
+                             "userID" : "\(currentUser.uid)",
+                             "userImage" : "",
+                             "userName" : "\(currentUser.displayName!)"])
+                        print("UserDataSaved")
+                    }
+                }
+                
                 let vc = UIStoryboard(name: "YewonStoryboard", bundle: nil).instantiateViewController(identifier: "YewonMainView")
                 vc.modalPresentationStyle = .fullScreen
                 self.currentUID = currentUser.uid
@@ -76,34 +86,6 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
             }//if else
         }//handle
     }//swipeGestured
-    
-    //btnTouched: swipe 안될 시 대비. 테스트용 추후 삭제 요망
-    @IBAction func btnTouched(_ sender: Any) {
-        print("touched")
-        //핸들로 로그인 했는지 안했는지 추적
-        handle = Auth.auth().addStateDidChangeListener { (auth, user) in
-            //만약 로그인 했다면
-            if let currentUser = auth.currentUser {
-                //currentUser 정보 넘겨주기
-                print(currentUser.displayName)
-                //로그인한 유저 -> 다음 뷰로 넘어감
-                let vc = UIStoryboard(name: "YujinStoryboard", bundle: nil).instantiateViewController(identifier: "LoggedView")
-                vc.modalPresentationStyle = .fullScreen
-                self.present(vc, animated: false, completion:  nil)
-            } else {
-                //로그인 안한 유저 -> 로그인 화면이 뜸
-                self.authUI!.delegate = self
-                  let providers: [FUIAuthProvider] = [
-                  FUIGoogleAuth()
-                ]
-                self.authUI!.providers = providers
-                let authViewController = self.authUI!.authViewController()
-                authViewController.modalPresentationStyle = .fullScreen
-                authViewController.setNavigationBarHidden(true, animated: false)
-                self.present(authViewController, animated: true, completion: nil)
-            }//if else
-        }//handle
-    }//btnTouched
     
     //logoutbtnTouched
     @IBAction func logoutbtnTouched(_ sender: Any) {
